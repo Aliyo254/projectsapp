c07a5603220fdda638fd93661ca37a7f4a6e37c8
Aliyo254 <alinurali254@gmail.com>
2022-06-14 14:44:57 +0300

    Project model

--- rating/models.py
+++ rating/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Project(models.Model):
+    title=models.CharField(max_length=100)
+    description=models.CharField(max_length=250)
+    link=models.URLField(max_length=100)
+    project_image=models.ImageField(upload_to='projects/')
+    owner=models.ForeignKey(User,on_delete=models.CASCADE)
