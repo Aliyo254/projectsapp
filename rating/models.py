bcfb9e2715b4eb91ae17b1aee6b352f0c60ad823
Aliyo254 <alinurali254@gmail.com>
2022-06-14 22:51:04 +0300

    Rating model

--- rating/models.py
+++ rating/models.py
@@ -20,3 +20,21 @@ class Profile(models.Model):
     @classmethod
     def search_by_profile(cls,search_term):
         profiles=cls.objects.filter(user__icontains=search_term)
+class Rate(models.Model):
+    RATING_CHOICES = (
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10,'10'),
+    )
+    project=models.ForeignKey(Project,on_delete=models.CASCADE)
+    rater=models.ForeignKey(User,on_delete=models.CASCADE)
+    design_rating = models.IntegerField(choices=RATING_CHOICES,null=True)
+    usability_rating=models.IntegerField(choices=RATING_CHOICES,null=True)
+    content_rating=models.IntegerField(choices=RATING_CHOICES,null=True)
